01a9e5a14881e743c5006047684f5ef55e2cfaf8
Yurii Tern <ternxstyle@gmail.com>
2022-04-16 23:56:50 +0300

    Added Filter (jQuery)

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,30 @@
-import * as flsFunctions from "./modules/functions.js";
+import * as flsFunctions from './modules/functions.js'
 
-flsFunctions.isWebp();
+flsFunctions.isWebp()
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
+
+$(function () {
+	let filter = $('[data-filter]')
+	filter.on('click', function (event) {
+		event.preventDefault()
+		let cat = $(this).data('filter')
+
+		if (cat == 'all') {
+			$('[data-cat]').removeClass('hide')
+		} else {
+			$('[data-cat]').each(function () {
+				let workCat = $(this).data('cat')
+
+				if (workCat != cat) {
+					$(this).addClass('hide')
+				} else {
+					$(this).removeClass('hide')
+				}
+			})
+		}
+	})
+})
