224672f186f8a0fbfe9e7ca914e2d94cb5b7966c
Yurii Tern <ternxstyle@gmail.com>
2022-04-30 02:08:12 +0300

    Change Intro + menu links

--- src/js/app.js
+++ src/js/app.js
@@ -123,6 +123,26 @@ navToggle.on('click', function (event) {
 })
 /*==========================================*/
 
+//Button Up
+// function backToTop() {
+// 	let button = $('.back-to-top')
+
+// 	$(window).on('scroll', () => {
+// 		if ($(this).scrollTop() >= 50) {
+// 			button.fadeIn()
+// 		} else {
+// 			button.fadeOut()
+// 		}
+// 	})
+// 	button.on('click', (e) => {
+// 		e.preventDefault()
+// 		$('html').animate({ scrollTop: 0 }, 1000)
+// 	})
+// }
+
+// backToTop()
+/*==========================================*/
+
 //Close modal window with button "Esc" (JS)
 // document.addEventListener('keydown', function (e) {
 // 	if (e.key === 'Escape') {
