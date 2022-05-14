0380d6d2461d82313fce7930896c4daa08a63553
Yurii Tern <ternxstyle@gmail.com>
2022-05-14 06:17:46 +0300

    'Read more' function - fixing

--- src/js/modules/read-more.js
+++ src/js/modules/read-more.js
@@ -1,19 +1,35 @@
-$(function () {
-	let h = 170,
-		t = $('#article'),
-		max = t[0].scrollHeight,
-		min = 92
-	$('.read-next').on('click', function (e) {
-		let H = t.height()
-		if (H === max) {
-			H = min
-		} else if (H + h > max) {
-			H = max
-		} else {
-			H += h
-		}
-		t.height(H)
-		$(this).text(H == max ? 'Hide' : 'Read more')
-		return false
-	})
-})
+export function readMore1() {
+	let dots1 = document.getElementById('dots1')
+	let more1 = document.getElementById('more1')
+	let btn1 = document.getElementById('btn1')
+
+	if (dots1.style.display === 'none') {
+		dots1.style.display = 'inline'
+		btn1.innerHTML = 'Read More'
+		more1.style.display = 'none'
+	} else {
+		dots1.style.display = 'none'
+		btn1.innerHTML = 'Hide'
+		more1.style.display = 'inline'
+	}
+}
+
+btn1.onclick = readMore1
+
+export function readMore2() {
+	let dots2 = document.getElementById('dots2')
+	let more2 = document.getElementById('more2')
+	let btn2 = document.getElementById('btn2')
+
+	if (dots2.style.display === 'none') {
+		dots2.style.display = 'inline'
+		btn2.innerHTML = 'Read More'
+		more2.style.display = 'none'
+	} else {
+		dots2.style.display = 'none'
+		btn2.innerHTML = 'Hide'
+		more2.style.display = 'inline'
+	}
+}
+
+btn2.onclick = readMore2
