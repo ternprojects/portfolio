33367cc6fad16d6148589efeb0e5f5c530f08cbf
Yurii Tern <ternxstyle@gmail.com>
2022-05-15 05:33:59 +0300

    'Read more' function - fixing_1

--- src/js/modules/read-more.js
+++ src/js/modules/read-more.js
@@ -33,3 +33,111 @@ export function readMore2() {
 }
 
 btn2.onclick = readMore2
+
+export function readMore3() {
+	let dots3 = document.getElementById('dots3')
+	let more3 = document.getElementById('more3')
+	let btn3 = document.getElementById('btn3')
+
+	if (dots3.style.display === 'none') {
+		dots3.style.display = 'inline'
+		btn3.innerHTML = 'Read More'
+		more3.style.display = 'none'
+	} else {
+		dots3.style.display = 'none'
+		btn3.innerHTML = 'Hide'
+		more3.style.display = 'inline'
+	}
+}
+
+btn3.onclick = readMore3
+
+export function readMore4() {
+	let dots4 = document.getElementById('dots4')
+	let more4 = document.getElementById('more4')
+	let btn4 = document.getElementById('btn4')
+
+	if (dots4.style.display === 'none') {
+		dots4.style.display = 'inline'
+		btn4.innerHTML = 'Read More'
+		more4.style.display = 'none'
+	} else {
+		dots4.style.display = 'none'
+		btn4.innerHTML = 'Hide'
+		more4.style.display = 'inline'
+	}
+}
+
+btn4.onclick = readMore4
+
+export function readMore5() {
+	let dots5 = document.getElementById('dots5')
+	let more5 = document.getElementById('more5')
+	let btn5 = document.getElementById('btn5')
+
+	if (dots5.style.display === 'none') {
+		dots5.style.display = 'inline'
+		btn5.innerHTML = 'Read More'
+		more5.style.display = 'none'
+	} else {
+		dots5.style.display = 'none'
+		btn5.innerHTML = 'Hide'
+		more5.style.display = 'inline'
+	}
+}
+
+btn5.onclick = readMore5
+
+export function readMore6() {
+	let dots6 = document.getElementById('dots6')
+	let more6 = document.getElementById('more6')
+	let btn6 = document.getElementById('btn6')
+
+	if (dots6.style.display === 'none') {
+		dots6.style.display = 'inline'
+		btn6.innerHTML = 'Read More'
+		more6.style.display = 'none'
+	} else {
+		dots6.style.display = 'none'
+		btn6.innerHTML = 'Hide'
+		more6.style.display = 'inline'
+	}
+}
+
+btn6.onclick = readMore6
+
+export function readMore7() {
+	let dots7 = document.getElementById('dots7')
+	let more7 = document.getElementById('more7')
+	let btn7 = document.getElementById('btn7')
+
+	if (dots7.style.display === 'none') {
+		dots7.style.display = 'inline'
+		btn7.innerHTML = 'Read More'
+		more7.style.display = 'none'
+	} else {
+		dots7.style.display = 'none'
+		btn7.innerHTML = 'Hide'
+		more7.style.display = 'inline'
+	}
+}
+
+btn7.onclick = readMore7
+
+export function readMore() {
+	let dots = document.getElementById('dots')
+	let more = document.getElementById('more')
+	let btn = document.getElementById('btn')
+
+	if (dots.style.display === 'none') {
+		dots.style.display = 'inline'
+		btn.innerHTML = 'See more projects...'
+		more.style.display = 'none'
+	} else {
+		dots.style.display = 'none'
+		btn.innerHTML = 'Hide'
+		more.style.display = 'inline'
+	}
+}
+
+btn.onclick = readMore
