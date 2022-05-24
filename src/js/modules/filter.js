af77f7876cd13d2cc18e8c609a9247c2d7362b43
Yurii Tern <ternxstyle@gmail.com>
2022-05-25 02:59:40 +0300

    Change filter function

--- src/js/modules/filter.js
+++ src/js/modules/filter.js
@@ -1,7 +1,7 @@
-/*Filter*/
+//Filter
 let filter = $('[data-filter]')
-filter.on('click', function (event) {
-	event.preventDefault()
+filter.on('click', function (e) {
+	e.preventDefault()
 	let cat = $(this).data('filter')
 
 	if (cat === 'all') {
@@ -17,4 +17,65 @@ filter.on('click', function (event) {
 		})
 	}
 })
+/*==========================================*/
+$(document).ready(function () {
+	//Year filter
+	let filterYear = $('[data-filter-year]')
+	filterYear.on('click', function (e) {
+		e.preventDefault()
+		let year = $(this).data('filterYear')
+
+		if (year === 'all') {
+			$('[data-year]').removeClass('hide')
+		} else {
+			$('[data-year]').each(function () {
+				let workYear = $(this).data('year')
+				if (workYear != year) {
+					$(this).addClass('hide')
+				} else {
+					$(this).removeClass('hide')
+				}
+			})
+		}
+	})
+	//Year filter (my)
+	filterYear.on('click', function (e) {
+		e.preventDefault()
+		let year = $(this).data('filterYear')
+
+		if (year === 'all') {
+			$('[data-year]').removeClass('invisible')
+		} else {
+			$('[data-year]').each(function () {
+				let workYear = $(this).data('year')
+				if (workYear != year) {
+					$(this).addClass('invisible')
+				} else {
+					$(this).removeClass('invisible')
+				}
+			})
+		}
+	})
+})
+
+/*==========================================*/
+//Hide
+// let filterHide = $('[data-filter-hide]')
+// filterHide.on('click', function (event) {
+// 	event.preventDefault()
+// 	let hide = $(this).data('filter')
+
+// 	if (hide === 'all') {
+// 		$('[data-cat-hide]').removeClass('hide')
+// 	} else {
+// 		$('[data-cat-hide]').each(function () {
+// 			let workCat = $(this).data('cat')
+// 			if (workCat != hide) {
+// 				$(this).addClass('hide')
+// 			} else {
+// 				$(this).removeClass('hide')
+// 			}
+// 		})
+// 	}
+// })
 /*==========================================*/
