c4db43248aab29f4c2d841339c93a2161be77b79
Y.Tern <ternxstyle@gmail.com>
2022-12-17 19:20:23 +0200

    Added SQL-Sertificste

---
+++ src/js/modules/data-qa.js
@@ -0,0 +1,21 @@
+// export let info = [
+// 	{
+// 		name: '<a href = "http://www.google.com" target="_blank">Google</a>',
+// 		surname: 'surname1',
+// 		age: 30,
+// 	},
+// 	{ name: 'name2', surname: 'surname2', age: 30 },
+// 	{ name: 'name3', surname: 'surname3', age: 30 },
+// 	{ name: 'name4', surname: 'surname4', age: 30 },
+// 	{ name: 'name5', surname: 'surname5', age: 30 },
+// 	{ name: 'name6', surname: 'surname6', age: 30 },
+// 	{ name: 'name7', surname: 'surname7', age: 30 },
+// 	{ name: 'name8', surname: 'surname8', age: 30 },
+// 	{ name: 'name9', surname: 'surname9', age: 30 },
+// 	{ name: 'name10', surname: 'surname10', age: 30 },
+// 	{ name: 'name11', surname: 'surname11', age: 30 },
+// 	{ name: 'name12', surname: 'surname12', age: 30 },
+// 	{ name: 'name13', surname: 'surname13', age: 30 },
+// 	{ name: 'name14', surname: 'surname14', age: 30 },
+// 	{ name: 'name15', surname: 'surname15', age: 30 },
+// ]
